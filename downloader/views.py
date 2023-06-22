6d4afb16aaa9a6cd7a31163ec8c8a3579583f200
oasiiss <oasiiss34@gmail.com>
2023-06-22 12:21:32 +0300

    download template create

--- downloader/views.py
+++ downloader/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def ConvertVideoPageViews(request):
+    return render(request, "ytb_download.html")
+
+def ConvertVideoViews(request):
+    url = request.GET.get("url")
+    format = request.GET.get("format")
+    print(url)
+    
+
