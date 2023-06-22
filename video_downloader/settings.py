4c6a05a853c3bc8aa3048c51a8c2c785088eae7d
oasiiss <oasiiss34@gmail.com>
2023-06-22 12:39:28 +0300

    asgi/channels settings done

--- video_downloader/settings.py
+++ video_downloader/settings.py
@@ -20,14 +20,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'downloader',
 ]
+
+ASGI_APPLICATION = 'video_downloader.asgi,application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
