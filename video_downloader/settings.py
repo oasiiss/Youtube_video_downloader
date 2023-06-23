73c734f3a64e0c12da9976b14ee3bf4c72372205
oasiiss <oasiiss34@gmail.com>
2023-06-23 09:54:34 +0300

    allowed host settings update

--- video_downloader/settings.py
+++ video_downloader/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["www.osmanefekurt.site", "osmanefekurt.site"]
+ALLOWED_HOSTS = ["www.osmanefekurt.site", "osmanefekurt.site", "34.224.212.102"]
 
 
 # Application definition
