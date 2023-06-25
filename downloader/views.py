1f97a9923f65e30535eba16ed96f76ebf3f0d6ca
oasiiss <oasiiss34@gmail.com>
2023-06-25 20:47:51 +0300

    template updated

--- downloader/views.py
+++ downloader/views.py
@@ -5,9 +5,3 @@ from django.http import HttpResponse
 def ConvertVideoPageViews(request):
     return render(request, "ytb_download.html")
 
-def ConvertVideoViews(request):
-    url = request.GET.get("url")
-    format = request.GET.get("format")
-    print(url)
-    
-
